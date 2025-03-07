2bed0a205ede62ff43be13cd2f6fdd330f326ee6
AbdelMe <simoelabdellaoui08@gmail.com>
2025-03-07 23:59:17 +0000

    add services and dossiers controller/model/migration

---
+++ database/migrations/2025_03_07_235350_create_dossiers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dossiers', function (Blueprint $table) {
+            $table->id('IDDossier');
+            $table->bigInteger('IDService');
+            $table->string('Dossier');
+            $table->year('Annee');
+            $table->foreign('IDService')->references('IDService')->on('services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dossiers');
+    }
+};
