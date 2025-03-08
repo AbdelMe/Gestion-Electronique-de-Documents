7b9638612f80777ec8437b9e5eac38eeabe20d34
AbdelMe <simoelabdellaoui08@gmail.com>
2025-03-08 00:48:28 +0000

    add add type_documents and type_rubriques controller/model/migration

---
+++ app/Http/Controllers/TypeRubriqueController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeRubrique;
+use App\Http\Requests\StoreTypeRubriqueRequest;
+use App\Http\Requests\UpdateTypeRubriqueRequest;
+
+class TypeRubriqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTypeRubriqueRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TypeRubrique $typeRubrique)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TypeRubrique $typeRubrique)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTypeRubriqueRequest $request, TypeRubrique $typeRubrique)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeRubrique $typeRubrique)
+    {
+        //
+    }
+}
