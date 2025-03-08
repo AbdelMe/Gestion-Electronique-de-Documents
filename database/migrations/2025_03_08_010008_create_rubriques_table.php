c1448e54a39eee1bfeddd2dc4f6f7023f630967e
AbdelMe <simoelabdellaoui08@gmail.com>
2025-03-08 01:03:02 +0000

    add add rubriques controller/model/migration

---
+++ database/migrations/2025_03_08_010008_create_rubriques_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rubriques', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('type_rubrique_id');
+            $table->unsignedBigInteger('type_document_id');
+            $table->string('Rubrique');
+            $table->string('Valeur');
+            $table->integer('Obligatoire');
+            $table->foreign('type_rubrique_id')->references('id')->on('type_rubriques');
+            $table->foreign('type_document_id')->references('id')->on('type_documents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rubriques');
+    }
+};
