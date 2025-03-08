9d421937a944e4adce4bb1b394df1bf68750baf0
AbdelMe <simoelabdellaoui08@gmail.com>
2025-03-08 01:06:33 +0000

    add add rubrique_documents controller/model/migration

---
+++ app/Http/Controllers/RubriqueDocumentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RubriqueDocument;
+use App\Http\Requests\StoreRubriqueDocumentRequest;
+use App\Http\Requests\UpdateRubriqueDocumentRequest;
+
+class RubriqueDocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRubriqueDocumentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RubriqueDocument $rubriqueDocument)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RubriqueDocument $rubriqueDocument)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRubriqueDocumentRequest $request, RubriqueDocument $rubriqueDocument)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RubriqueDocument $rubriqueDocument)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_03_08_010344_create_rubrique_documents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rubrique_documents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('rubrique_id');
+            $table->string('Valeur');
+            $table->unsignedBigInteger('document_id');
+            $table->foreign('rubrique_id')->references('id')->on('rubriques');
+            $table->foreign('document_id')->references('id')->on('documents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rubrique_documents');
+    }
+};
