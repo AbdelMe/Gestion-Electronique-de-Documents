621ec669d49c90c7719ece205c400eb6a65cbb23
AbdelMe <simoelabdellaoui08@gmail.com>
2025-03-15 14:58:31 +0000

    set up the pricipal design

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('layouts.head')
+</head>
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        @include('layouts.sidebar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_navbar.html -->
+            <nav class="navbar p-0 fixed-top d-flex flex-row">
+                <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+                    <a class="navbar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg"
+                            alt="logo" /></a>
+                </div>
+                @include('layouts.header')
+            </nav>
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('main')
+                </div>
+
+            </div>
+        </div>
+    </div>
+    @include('layouts.scripts')
+</body>
+</html>
