13eb3b55cd2d116d7f92ae1d332d6e0a9514e1a5
AbdelMe <simoelabdellaoui08@gmail.com>
2025-04-27 23:42:50 +0100

    redesign documents index with TailwindCSS and support light/dark modes

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -17,12 +17,13 @@
 @endsection
 
 @section('content')
-    <div class="d-flex justify-content-between align-items-center mb-4">
-        <h2 class="text-dark">Liste des Documents</h2>
-        <a href="{{ route('documents.create') }}" class="btn btn-success">
-            <i class="bi bi-plus-lg me-1"></i> Ajouter Document
-        </a>
-    </div>
+<div class="flex justify-between items-center mb-4">
+    <h2 class="text-2xl font-semibold text-gray-800 dark:text-white px-4">Liste des Documents</h2>
+    <a href="{{ route('documents.create') }}" class="inline-flex items-center px-4 py-2 bg-teal-400 hover:bg-teal-500 text-white text-sm font-medium rounded-md shadow-md">
+        <i class="bi bi-plus-lg me-1"></i> Ajouter Document
+    </a>
+</div>
+
     <livewire:search-document documents="$documents" />
 
 @endsection
