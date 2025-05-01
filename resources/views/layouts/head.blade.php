143bfa3d1f691c2a43a55eefbf99ec6b24ebd654
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-01 19:58:43 +0100

    remove all scss files

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,27 +1,31 @@
-
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>@yield('title')</title>
 <!-- plugins:css -->
-<link rel="stylesheet" href={{ asset("assets/vendors/mdi/css/materialdesignicons.min.css") }}>
-<link rel="stylesheet" href={{ asset("assets/vendors/css/vendor.bundle.base.css") }}>
+<link rel="stylesheet" href={{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}>
+<link rel="stylesheet" href={{ asset('assets/vendors/css/vendor.bundle.base.css') }}>
 <!-- endinject -->
 <!-- Plugin css for this page -->
-<link rel="stylesheet" href={{ asset("assets/vendors/jvectormap/jquery-jvectormap.css") }}>
-<link rel="stylesheet" href={{ asset("assets/vendors/flag-icon-css/css/flag-icon.min.css") }}>
-<link rel="stylesheet" href={{ asset("assets/vendors/owl-carousel-2/owl.carousel.min.css") }}>
-<link rel="stylesheet" href={{ asset("assets/vendors/owl-carousel-2/owl.theme.default.min.css") }}>
+<link rel="stylesheet" href={{ asset('assets/vendors/jvectormap/jquery-jvectormap.css') }}>
+<link rel="stylesheet" href={{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}>
+<link rel="stylesheet" href={{ asset('assets/vendors/owl-carousel-2/owl.carousel.min.css') }}>
+<link rel="stylesheet" href={{ asset('assets/vendors/owl-carousel-2/owl.theme.default.min.css') }}>
 <!-- End plugin css for this page -->
 <!-- inject:css -->
 <!-- endinject -->
 <!-- Layout styles -->
-<link rel="stylesheet" href={{ asset("assets/css/style.css") }}>
+<link rel="stylesheet" href={{ asset('assets/css/style.css') }}>
 <!-- End layout styles -->
-<link rel="shortcut icon" href={{ asset("assets/images/favicon.png") }} />
+<link rel="shortcut icon" href={{ asset('assets/images/favicon.png') }} />
 {{-- Bootstrap icons --}}
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 {{-- <link rel="stylesheet" href={{ asset('assets/vendors/select2/select2.min.css') }}> --}}
 {{-- <link rel="stylesheet" href={{ asset('assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css') }}> --}}
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+{{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
+{{-- <script defer src="//unpkg.com/alpinejs"></script> --}}
+
