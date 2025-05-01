5a0369376e4fae1364fbba904f795a5fb634e393
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-01 17:19:48 +0100

    update searchUser livewire class for send users with pagination

--- app/Livewire/SearchUser.php
+++ app/Livewire/SearchUser.php
@@ -5,38 +5,51 @@ namespace App\Livewire;
 use App\Models\Entreprise;
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 use Spatie\Permission\Models\Role;
 
 class SearchUser extends Component
 {
+    use WithPagination;
+
     public $selected_entreprise = "";
     public $withRol = "";
     public $withoutRol = "";
     public $searchUser = "";
+
+    // public function updating($field)
+    // {
+    //     $this->resetPage();
+    // }
+
     public function render()
     {
-        $users = [];
-        if(!empty($this->withRol)){
-            $users = User::role($this->withRol)->get();
-            // dd($users);
+        $query = User::query()->with('entreprise');
+
+        if (!empty($this->withRol)) {
+            $query->role($this->withRol);
         }
-        if(!empty($this->withoutRol)){
-            $users = User::withoutRole($this->withoutRol)->get();
+
+        if (!empty($this->withoutRol)) {
+            $query->withoutRole($this->withoutRol);
         }
-    
+
         if (!empty($this->selected_entreprise)) {
-            $users = User::with('entreprise')
-                ->where('entreprise_id', $this->selected_entreprise)
-                ->get();
+            $query->where('entreprise_id', $this->selected_entreprise);
         }
+
         if (!empty($this->searchUser)) {
-            $users = User::where('first_name', 'like', '%' . $this->searchUser . '%')
-            ->orWhere('last_name', 'like', '%' . $this->searchUser . '%')
-            ->get();        
+            $query->where(function ($q) {
+                $q->where('first_name', 'like', '%' . $this->searchUser . '%')
+                  ->orWhere('last_name', 'like', '%' . $this->searchUser . '%');
+            });
         }
+
+        $users = $query->paginate(5);
+
         $entreprises = Entreprise::all();
         $roles = Role::all();
-        $entreprises = Entreprise::all();
-        return view('livewire.search-user',compact('roles','entreprises','users'));
+
+        return view('livewire.search-user', compact('users', 'entreprises', 'roles'));
     }
 }
