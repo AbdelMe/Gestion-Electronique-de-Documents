82ff08820d530d2faf5782846c855c6892aac97a
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-02 15:04:36 +0100

    create and store some etats

--- database/seeders/EtatSeeder.php
+++ database/seeders/EtatSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Etat;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class EtatSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $etats = [
+            "Draft" => "yellow",
+            // "Pending Review",
+            // "Under Review",
+            "Approved" => "green",
+            "Rejected" => "red",
+            "Final" => "gray",
+            // "Published" => "",
+            // "Archived" => "",
+            // "Canceled" => ""
+        ];
+
+        foreach($etats as $etat => $color){
+            Etat::firstOrCreate(["etat" => $etat , "color" => $color]);
+        }
     }
 }
