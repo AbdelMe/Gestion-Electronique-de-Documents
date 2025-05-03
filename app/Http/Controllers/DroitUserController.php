ce9fd1876786994bd6203ae9b8b498eaec8752d1
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-03 18:04:17 +0100

    Create methods for managing user permissions - Implemented methods for associating users with their permissions. - Added functionality to store and update user permissions. - Added methods to delete one or all user permissions.

--- app/Http/Controllers/DroitUserController.php
+++ app/Http/Controllers/DroitUserController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\DroitUser;
 use App\Http\Requests\StoreDroitUserRequest;
 use App\Http\Requests\UpdateDroitUserRequest;
+use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DroitUserController extends Controller
 {
@@ -14,54 +16,70 @@ class DroitUserController extends Controller
      */
     public function index()
     {
-        //
+        $roles = Role::with('permissions')->paginate(5);
+        return view('user_permissions.index', compact('roles'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function assignPermitionsToRole()
     {
-        //
+        // $roles = Role::all();
+        // $permissions = Permission::all();
+        // $assignedPermissions = $role->permissions->pluck('id')->toArray();
+        return view('user_permissions.assignPermitionsToRole', [
+            'roles' => Role::all(),
+            'permissions' => Permission::all(),
+            'selectedRole' => null
+        ]);
+    }
+    public function storeAssignPermitionsToRole(Request $request)
+    {
+        // dd($request->role_id);
+        $role = Role::findOrFail($request->role_id);
+        foreach ($request->permissions as $key => $permission) {
+            $perm = Permission::where('id', $permission)->get();
+            $role->givePermissionTo($perm);
+        }
+        // $role->syncPermissions($request->permissions ?? []);
+        // $allPermissions = $role->getAllPermissions();
+
+        // dd($allPermissions);
+    }
+    public function editRolePermissions(Role $role)
+    {
+        $permissions = Permission::all()->groupBy('guard_name');
+        return view('user_permissions.editRolePermissions', compact('role', 'permissions'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreDroitUserRequest $request)
+
+    public function updateRolePermissions(Request $request, Role $role)
     {
-        //
+        $request->validate([
+            'permissions' => 'array',
+            'permissions.*' => 'exists:permissions,id',
+        ]);
+
+        $permissions = Permission::whereIn('id', $request->permissions ?? [])->get();
+
+        $role->syncPermissions($permissions);
+
+        return back()->with('success', 'Permissions updated.');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(DroitUser $droitUser)
+    public function deletePermition(Role $role, Permission $permission)
     {
-        //
+        // dd($permission);
+        $role->revokePermissionTo($permission);
+        return back()->with('success', 'Permissions Revoked.');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(DroitUser $droitUser)
+    public function revokeAllPermissions(Role $role)
     {
-        //
-    }
+        $role->syncPermissions([]);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateDroitUserRequest $request, DroitUser $droitUser)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(DroitUser $droitUser)
-    {
-        //
+        return redirect()->back()
+            ->with('success', 'All permissions revoked successfully');
     }
 }
