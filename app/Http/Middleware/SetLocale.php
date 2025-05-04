61129c81741f602926112d59aefb412530139ce6
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-04 18:14:36 +0100

    update local lange according the session

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -9,5 +9,16 @@ use Illuminate\Support\Facades\Session;
 
 class SetLocale
 {
-    
+    public function handle(Request $request, Closure $next)
+    {
+        $locale = $request->session()->get('locale', config('app.locale'));
+        
+        if (array_key_exists($locale, config('app.available_locales'))) {
+            app()->setLocale($locale);
+        } else {
+            app()->setLocale(config('app.fallback_locale'));
+        }
+        
+        return $next($request);
+    }
 }
