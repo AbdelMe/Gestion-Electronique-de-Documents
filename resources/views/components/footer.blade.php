1f31c9fb7dbeebb8143b224deed007110c8adb42
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-04 19:41:08 +0100

    create footer component view

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="w-full text-gray-800 py-4 bg-transparent dark:text-gray-300 mt-auto">
+    <div class="text-center">
+        <p class="text-sm">&copy; {{ date('Y') }} All rights reserved | Cette application est réalisée par Mohmmed El Abdellaoui</p>
+    </div>
+</footer>
