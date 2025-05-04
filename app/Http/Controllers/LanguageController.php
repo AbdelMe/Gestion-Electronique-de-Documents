36f4d9eaa1c201117b88bac59a0f0a5333af8681
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-04 18:16:20 +0100

    update the LanguageController to change the session by the selected lang

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -1,10 +1,19 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
 
 class LanguageController extends Controller
 {
-    //
+    public function switch($locale)
+    {
+        if (!array_key_exists($locale, config('app.available_locales'))) {
+            $locale = config('app.fallback_locale');
+        }
+        
+        Session::put('locale', $locale);
+        return redirect()->back();
+    }
 }
