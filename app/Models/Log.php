3703ae89bb983d4b33a263ab4549a565ce549480
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-05 11:27:04 +0100

    add relationships between models for Log Model

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Log extends Model
 {
     protected $fillable = ['document_id', 'user_id', 'date',"action"];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function document(){
+        return $this->belongsTo(Document::class);
+    }
 }
