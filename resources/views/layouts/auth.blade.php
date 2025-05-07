a73ab874a692817a89ba91be22179cdff2404855
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-07 23:14:53 +0100

    update auth view & remove bootstrap cdn & keep tailwind

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,21 +5,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel 11 Custom User Login Page - itsolutionstuff.com</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" --}}
     <style type="text/css">
         body {
             background: #F8F9FA;
         }
     </style>
+    @include('layouts.head')
 </head>
 
-<body>
+<body class="overflow-hidden">
 
-    <!-- Content Section -->
     @yield('content')
 
-    <!-- Footer Links -->
     <div class="text-center mt-4">
         @yield('auth-footer')
     </div>
