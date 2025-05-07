c8b4a07b708eba9a99286acb5866aac1107573c4
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-07 23:00:53 +0100

    implement search by Etat , classe, TypeDocument, RubriqueDocument, Rubrique

--- app/Livewire/SearchBar.php
+++ app/Livewire/SearchBar.php
@@ -8,23 +8,40 @@ use Livewire\Component;
 class SearchBar extends Component
 {
     public string $search_doc = "";
-
     public function render()
     {
         $docs = [];
-        
-        if(strlen($this->search_doc) >= 2) {
-            $docs = Document::with(['dossier', 'typeDocument'])
-                ->where(function($query) {
-                    $query->where('titre', 'LIKE', '%'.$this->search_doc.'%')
-                          ->orWhereHas('dossier', function($q) {
-                              $q->where('Dossier', 'LIKE', '%'.$this->search_doc.'%');
-                          });
+        if (strlen($this->search_doc) >= 2) {
+            $docs = Document::with(['dossier', 'typeDocument', 'RubriqueDocument'])
+                ->where(function ($query) {
+                    $query->where('titre', 'LIKE', '%' . $this->search_doc . '%')
+                        ->orWhereHas('dossier', function ($q) {
+                            $q->where('Dossier', 'LIKE', '%' . $this->search_doc . '%')
+                                ->orWhereHas('entreprise', function ($q) {
+                                    $q->where('NomClient', 'LIKE', '%' . $this->search_doc . '%');
+                                });
+                        })->orWhereHas('Etat', function ($q) {
+                            $q->where('etat', 'LIKE', '%' . $this->search_doc . '%');
+                        })->orWhereHas('classe', function ($q) {
+                            $q->where('classe', 'LIKE', '%' . $this->search_doc . '%');
+                        })->orWhereHas('TypeDocument', function ($q) {
+                            $q->where('TypeDocument', 'LIKE', '%' . $this->search_doc . '%');
+                        })->orWhereHas('RubriqueDocument', function ($q) {
+                            $q->where('valeur', 'LIKE', '%' . $this->search_doc . '%')
+                            ->orWhereHas('Rubrique', function ($q) {
+                                $q->where('Rubrique', 'LIKE', '%' . $this->search_doc . '%'); // Adjust 'name' to your RubriqueDocument field
+                            });
+                        });
+                })->orWhere(function ($query) {
+                    $query->where('metadata', 'LIKE', '%' . $this->search_doc . '%');
+                })->orWhere(function ($query) {
+                    $query->where('tag', 'LIKE', '%' . $this->search_doc . '%');
+                })->orWhere(function ($query) {
+                    $query->where('CheminDocument', 'LIKE', '%' . $this->search_doc . '%');
                 })
                 // ->limit(10)
                 ->get();
         }
-
         return view('livewire.search-bar', [
             'docs' => $docs
         ]);
