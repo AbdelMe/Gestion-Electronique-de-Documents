5cad4639e7ed5273ddf7730017b7d59d59cb282c
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-09 23:21:27 +0100

    send notifications for every view for skip the bug of $notifications doesn't exist(because notification in the header view)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Notification;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,34 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Paginator::useBootstrapFive();
+        View::composer('layouts.header', function ($view) {
+            if (auth()->check()) {
+                $notifications = Notification::where('user_id', auth()->id())
+                    ->latest()
+                    ->take(10)
+                    ->get();
+                $view->with('notifications', $notifications);
+            }
+
+        });
+        // View::composer('layouts.header', function ($view) {
+        //     if (auth()->check()) {
+        //         $user = auth()->user();
+                
+        //         if ($user->hasRole('admin')) {
+        //             $notifications = Notification::latest()
+        //                 ->take(10)
+        //                 ->get();
+        //         } 
+        //         else {
+        //             $notifications = Notification::where('user_id', $user->id)
+        //                 ->latest()
+        //                 ->take(10)
+        //                 ->get();
+        //         }
+                
+        //         $view->with('notifications', $notifications);
+        //     }
+        // });
     }
 }
