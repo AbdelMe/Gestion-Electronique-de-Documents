e958da2d10b3674a582bf298ce36d628510e6f13
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-11 19:18:38 +0100

    Update showUserLog.blade.php to display detailed user activity logs

--- resources/views/logs/showUsersLog.blade.php
+++ resources/views/logs/showUsersLog.blade.php
@@ -30,20 +30,25 @@
                             <td class="px-6 py-4 border-b dark:border-gray-800">{{ $log->user->first_name }}
                                 {{ $log->user->last_name }}</td>
                             <td class="px-6 py-4 border-b dark:border-gray-800">
-                                {{ $log->document->titre }}
-                                <a href="{{ route('documents.show', $log->document->id) }}"
-                                    class="ml-2 text-gray-500 hover:text-blue-500 dark:hover:text-blue-400"
-                                    title="View Document">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                    </svg>
-                                </a>
+                                @if ($log->document)
+                                    {{ $log->document->titre }}
+                                    <a href="{{ route('documents.show', $log->document->id) }}"
+                                        class="ml-2 text-gray-500 hover:text-blue-500 dark:hover:text-blue-400"
+                                        title="View Document">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                        </svg>
+                                    </a>
+                                @else
+                                    <span class="text-red-500">Document deleted</span>
+                                @endif
                             </td>
-                            <td class="px-6 py-4 border-b dark:border-gray-800">{{ \Carbon\Carbon::parse($log->date)->format('D d M, Y') }}                            </td>
+                            <td class="px-6 py-4 border-b dark:border-gray-800">
+                                {{ \Carbon\Carbon::parse($log->date)->format('D d M, Y') }} </td>
                             <td class="px-6 py-4 border-b dark:border-gray-800">{{ $log->action }}</td>
                         </tr>
                     @endforeach
