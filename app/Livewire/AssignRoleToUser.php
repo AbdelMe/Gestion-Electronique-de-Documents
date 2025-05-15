32a8eec47e54ea4725c85348338c2483fe44b6a4
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-15 21:29:15 +0100

    create livewire class for this operations

---
+++ app/Livewire/AssignRoleToUser.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Entreprise;
+use App\Models\User;
+use Livewire\Component;
+use Spatie\Permission\Models\Role;
+
+class AssignRoleToUser extends Component
+{
+    public $selected_entreprise = "";
+    public $searchUser = "";
+    public function render()
+    {
+        $query = User::query()->with('entreprise');
+
+        if (!empty($this->withRol)) {
+            $query->role($this->withRol);
+        }
+
+
+        if (!empty($this->selected_entreprise)) {
+            $query->where('entreprise_id', $this->selected_entreprise);
+        }
+
+        if (!empty($this->searchUser)) {
+            $query->where(function ($q) {
+                $q->where('first_name', 'like', '%' . $this->searchUser . '%')
+                    ->orWhere('last_name', 'like', '%' . $this->searchUser . '%')
+                    ->orWhere('email', 'like', '%' . $this->searchUser . '%');
+            });
+        }
+
+        $users = $query->paginate(5);
+        $entreprises = Entreprise::all();
+        $roles = Role::all();
+        return view('livewire.assign-role-to-user',compact('users','entreprises','roles'));
+    }
+}
