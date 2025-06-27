12b86dc11f9a5e243f809ace23188f9a3059e6ac
AbdelMe <simoelabdellaoui08@gmail.com>
2025-06-27 22:45:13 +0100

    feat: add view link for documents in etat-document-search

--- resources/views/livewire/etat-document-search.blade.php
+++ resources/views/livewire/etat-document-search.blade.php
@@ -101,6 +101,17 @@
                                                 d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                         </svg>
                                     </a>
+<a href="{{ route('documents.show', $document->id) }}"
+    class="text-green-600 hover:text-green-900 dark:text-green-400 dark:hover:text-green-300 transition-colors duration-200"
+    title="Afficher">
+    <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M2.458 12C3.732 7.943 7.523 5 12 5s8.268 2.943 9.542 7c-1.274 4.057-5.065 7-9.542 7s-8.268-2.943-9.542-7z" />
+    </svg>
+</a>
+
                                     <form action="{{ route('documents.destroy', $document->id) }}" method="POST"
                                         onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer ce document ?');">
                                         @csrf
