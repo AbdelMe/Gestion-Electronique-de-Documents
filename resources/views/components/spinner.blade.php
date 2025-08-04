cc90e6c46dad93e56b8604b08f49fa749ef70218
AbdelMe <simoelabdellaoui08@gmail.com>
2025-08-04 20:05:19 +0100

    Add spinner component for export loading indication

---
+++ resources/views/components/spinner.blade.php
@@ -0,0 +1,4 @@
+<div class="flex justify-center items-center h-64">
+    <div class="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"></div>
+    <span class="ml-4 text-gray-600 text-lg">Exporting...</span>
+</div>
