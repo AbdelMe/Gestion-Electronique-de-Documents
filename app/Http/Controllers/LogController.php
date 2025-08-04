56603bf453d34321c64545f7729c13cbb421c852
AbdelMe <simoelabdellaoui08@gmail.com>
2025-08-04 20:04:00 +0100

    Refactor showUsersLog method to support export functionality and improve log retrieval logic

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Log;
 use App\Http\Requests\StoreLogRequest;
 use App\Http\Requests\UpdateLogRequest;
+use Illuminate\Http\Request;
 
 class LogController extends Controller
 {
-    public function showUsersLog(){
-        $logs = Log::paginate(5);
-        return view('logs.showUsersLog',compact('logs'));
+    public function showUsersLog(Request $request)
+    {
+        if ($request->has('export')) {
+            // No pagination if exporting
+            $logs = Log::all();
+        } else {
+            // Regular paginated view
+            $logs = Log::paginate(5);
+        }
+
+        return view('logs.showUsersLog', compact('logs'));
     }
 }
